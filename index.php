c186928145fc812344e5cbe7c34c6e8c0551674c
msaheb <malik.saheb@gmail.com>
2017-05-18 09:53:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <div style="display:block;margin-left:auto;margin-right:auto;text-align:center;">
             <h2>Welcome to you quick demo application running on</h2>
         </div>
-	<div style="width:400px;height:200px;overflow:hidden;display:block;margin-left:auto;margin-right:auto;">
+	<div style="width:400px;height:100px;overflow:hidden;display:block;margin-left:auto;margin-right:auto;">
             <img src="openshift.png" width="400px" height="auto"/>
         </div>
 	
@@ -19,25 +19,6 @@
 		<p> The server IP <b><?php echo $_SERVER['SERVER_ADDR'] ?></b>
 		    and the port number is <b><?php echo $_SERVER['SERVER_PORT'] ?> </b></p>
         </div>
-	    <h2>Application Information</h2>
-	    <p> The server IP <?php echo $_SERVER['SERVER_ADDR'] ?> and the port nulber is <?php echo $_SERVER['SERVER_PORT'] ?> </p>
-          <table table align="center>
-            <thead>
-              <tr>
-                <th>Env Var</th>
-                <th>Value</th>
-              <tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Pod IP</td>
-                <td><?php echo $_SERVER['SERVER_ADDR'] ?></td>
-              </tr>
-              <tr>
-                <td>Pod Port</td>
-                <td><?php echo $_SERVER['SERVER_PORT'] ?></td>
-              </tr>
-            </tbody>
-          </table>
+
     </body>
 </html>
